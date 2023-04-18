00a2d1f38e9e4152f5a636813ef8613d8830c484
Jefferson <jefferson.silvaquinto@gmail.com>
2023-04-17 23:25:02 -0500

    first commit

---
+++ src/main/java/com/app/thecipriani/man.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.app.thecipriani;
+
+import com.app.view.menuMain;
+import java.awt.Frame;
+
+/**
+ *
+ * @author PC
+ */
+public class man {
+    
+    public static void main(String[] args) {
+        menuMain menu = new menuMain();        
+        menu.setLocation(0,0);
+        menu.setVisible(true);   
+    }    
+}
